dc4f6a270064825a9b8594a81bd8434922137a55
MalcyL <malcolm@landonsonline.me.uk>
2017-11-12 21:03:43 +0000

    New build

--- dist/proponojs.js
+++ dist/proponojs.js
@@ -1,13 +1,13 @@
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
-		module.exports = factory(require("aws-sdk"), require("shortid"));
+		module.exports = factory(require("aws-sdk"), require("sqs-consumer"), require("shortid"));
 	else if(typeof define === 'function' && define.amd)
-		define(["aws-sdk", "shortid"], factory);
+		define(["aws-sdk", "sqs-consumer", "shortid"], factory);
 	else if(typeof exports === 'object')
-		exports["proponojs"] = factory(require("aws-sdk"), require("shortid"));
+		exports["proponojs"] = factory(require("aws-sdk"), require("sqs-consumer"), require("shortid"));
 	else
-		root["proponojs"] = factory(root["aws-sdk"], root["shortid"]);
-})(this, function(__WEBPACK_EXTERNAL_MODULE_2__, __WEBPACK_EXTERNAL_MODULE_3__) {
+		root["proponojs"] = factory(root["aws-sdk"], root["sqs-consumer"], root["shortid"]);
+})(this, function(__WEBPACK_EXTERNAL_MODULE_2__, __WEBPACK_EXTERNAL_MODULE_3__, __WEBPACK_EXTERNAL_MODULE_4__) {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
@@ -57,33 +57,30 @@ return /******/ (function(modules) { // webpackBootstrap
 	'use strict';
 	
 	exports.proponojs = __webpack_require__(1);
-	exports.env_config = __webpack_require__(4);
+	exports.env_config = __webpack_require__(5);
 
 /***/ }),
 /* 1 */
 /***/ (function(module, exports, __webpack_require__) {
 
 	const AWS = __webpack_require__(2);
+	const Consumer = __webpack_require__(3);
 	
-	const shortid = __webpack_require__(3);
+	const shortid = __webpack_require__(4);
 	
 	class ProponoJS {
 	  constructor(config) {
 	    this.config = config;
 	    AWS.config.update(config);
+	    this.sns = new AWS.SNS();
+	    this.sqs = new AWS.SQS();
 	  }
 	
-	  publish(topic, message, cb) { // eslint-disable-line class-methods-use-this
-	    const sns = new AWS.SNS();
-	    const createParams = {
-	      Name: topic,
-	    };
-	    sns.createTopic(createParams, (createErr, createData) => {
+	  publish(topic, message, cb) {
+	    this.createSnsTopic(topic, (createErr, topicArn) => {
 	      if (createErr) {
 	        cb(createErr);
 	      } else {
-	        const topicArn = createData.TopicArn;
-	
 	        const body = {
 	          id: shortid.generate(),
 	          message,
@@ -94,7 +91,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	          TopicArn: topicArn,
 	        };
 	
-	        sns.publish(publishParams, (publishErr, publishData) => {
+	        this.sns.publish(publishParams, (publishErr, publishData) => {
 	          if (publishErr) {
 	            cb(publishErr);
 	          } else {
@@ -103,6 +100,167 @@ return /******/ (function(modules) { // webpackBootstrap
 	        });
 	      }
 	    });
+	  }
+	
+	  listen(topic, processMessage) {
+	    this.createTopicQueueAndSubscription(topic, (createErr, queueUrl) => {
+	      if (createErr) {
+	        throw createErr;
+	      } else {
+	        const app = Consumer.create({
+	          queueUrl,
+	          handleMessage: (message, done) => {
+	            const body = JSON.parse(message.Body);
+	            const payload = JSON.parse(body.Message);
+	            processMessage(payload.message, done);
+	          },
+	          sqs: this.sqs,
+	        });
+	
+	        // app.on('error', (err) => {
+	        //   console.log(err.message);
+	        // });
+	
+	        app.start();
+	      }
+	    });
+	  }
+	
+	  createTopicQueueAndSubscription(topic, cb) {
+	    this.createSnsTopic(topic, (createSnsErr, topicArn) => {
+	      if (createSnsErr) {
+	        cb(createSnsErr);
+	      } else {
+	        this.createSqsQueue(topic, (createSqsErr, queueUrl, queueArn) => {
+	          if (createSqsErr) {
+	            cb(createSqsErr);
+	          } else {
+	            this.subscribeSqsToSns(topicArn, queueArn, (subscribeErr) => {
+	              if (subscribeErr) {
+	                cb(subscribeErr);
+	              } else {
+	                this.setSqsPolicy(topicArn, queueUrl, queueArn, (policyErr) => {
+	                  if (policyErr) {
+	                    cb(policyErr);
+	                  } else {
+	                    cb(null, queueUrl);
+	                  }
+	                });
+	              }
+	            });
+	          }
+	        });
+	      }
+	    });
+	  }
+	
+	  createSnsTopic(topic, cb) {
+	    const createParams = {
+	      Name: topic,
+	    };
+	    this.sns.createTopic(createParams, (createErr, createData) => {
+	      // console.log('Create Topic err: ' + JSON.stringify(createErr));
+	      // console.log('Create Topic data: ' + JSON.stringify(createData));
+	      if (createErr) {
+	        cb(createErr);
+	      } else {
+	        cb(null, createData.TopicArn);
+	      }
+	    });
+	  }
+	
+	  createSqsQueue(topic, cb) {
+	    const params = { QueueName: this.queueName(topic) };
+	    this.sqs.createQueue(params, (createErr, createData) => {
+	      // console.log('Create Queue err: ' + JSON.stringify(createErr));
+	      // console.log('Create Queue data: ' + JSON.stringify(createData));
+	      if (createErr) {
+	        cb(createErr);
+	      } else {
+	        this.getQueueAttributes(createData.QueueUrl, (getAttributesErr, queueArn) => {
+	          if (getAttributesErr) {
+	            cb(getAttributesErr);
+	          } else {
+	            cb(null, createData.QueueUrl, queueArn);
+	          }
+	        });
+	      }
+	    });
+	  }
+	
+	  getQueueAttributes(queueUrl, cb) {
+	    const params = {
+	      QueueUrl: queueUrl,
+	      AttributeNames: [
+	        'QueueArn',
+	      ],
+	    };
+	    this.sqs.getQueueAttributes(params, (getErr, getData) => {
+	      if (getErr) {
+	        cb(getErr);
+	      } else {
+	        // console.log('Get Queue data: ' + JSON.stringify(getData));
+	        cb(null, getData.Attributes.QueueArn);
+	      }
+	    });
+	  }
+	
+	  subscribeSqsToSns(topicArn, queueArn, cb) {
+	    const params = {
+	      Protocol: 'sqs',
+	      TopicArn: topicArn,
+	      Endpoint: queueArn,
+	    };
+	    this.sns.subscribe(params, (err) => {
+	      if (err) {
+	        cb(err);
+	      } else {
+	        cb();
+	      }
+	    });
+	  }
+	
+	  setSqsPolicy(topicArn, queueUrl, queueArn, cb) {
+	    const params = {
+	      QueueUrl: queueUrl,
+	      Attributes: {
+	        Policy: JSON.stringify(this.generatePolicy(topicArn, queueArn)),
+	      },
+	    };
+	    this.sqs.setQueueAttributes(params, (err) => {
+	      if (err) {
+	        cb(err);
+	      } else {
+	        cb();
+	      }
+	    });
+	  }
+	
+	  generatePolicy(topicArn, queueArn) { // eslint-disable-line class-methods-use-this
+	    return {
+	      Version: '2008-10-17',
+	      Id: `${queueArn}/SQSDefaultPolicy`,
+	      Statement: [
+	        {
+	          Sid: `${queueArn}-Sid`,
+	          Effect: 'Allow',
+	          Principal: {
+	            AWS: '*',
+	          },
+	          Action: 'SQS:*',
+	          Resource: `${queueArn}`,
+	          Condition: {
+	            StringEquals: {
+	              'aws:SourceArn': `${topicArn}`,
+	            },
+	          },
+	        },
+	      ],
+	    };
+	  }
+	
+	  queueName(topic) {
+	    return `${this.config.applicationName}-${topic}${this.config.queueSuffix}`;
 	  }
 	}
 	
@@ -122,15 +280,23 @@ return /******/ (function(modules) { // webpackBootstrap
 /* 3 */
 /***/ (function(module, exports) {
 
-	module.exports = require("shortid");
+	module.exports = require("sqs-consumer");
 
 /***/ }),
 /* 4 */
 /***/ (function(module, exports) {
 
+	module.exports = require("shortid");
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports) {
+
 	const accessKey = process.env.PROPONOJS_AWS_ACCESS_KEY_ID;
 	const secretKey = process.env.PROPONOJS_AWS_SECRET_KEY;
 	const queueRegion = process.env.PROPONOJS_QUEUE_REGION;
+	const applicationName = process.env.PROPONOJS_APPLICATION_NAME;
+	const queueSuffix = process.env.PROPONOJS_QUEUE_SUFFIX;
 	
 	// The following are the complete set of Propono configuration
 	// parameters. These are not yet supported by ProponoJS
@@ -145,6 +311,8 @@ return /******/ (function(modules) { // webpackBootstrap
 	  accessKeyId: accessKey,
 	  secretAccessKey: secretKey,
 	  region: queueRegion,
+	  applicationName,
+	  queueSuffix,
 	};
 	
 	module.exports = config;
